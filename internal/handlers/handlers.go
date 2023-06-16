971d41d6c56748248db95b7ad9f5f6f30a79ba48
selim <salimmia3745@gmail.com>
2023-06-17 01:33:00 +0600

    Providing feedback when searching by room, and connecting to the reservation pag (114)

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -242,8 +242,11 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 }
 
 type jsonResponse struct {
-	OK      bool   `json:"ok"`
-	Message string `json:"message"`
+	OK      	bool   	`json:"ok"`
+	Message 	string 	`json:"message"`
+	RoomID  	string 	`json:"room_id"`
+	StartDate 	string	`json:"start_date"`
+	EndDate		string	`json:"end_date"`
 }
 
 // AvailabilityJSON handles request for availability and sends JSON response
@@ -263,6 +266,9 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	resp := jsonResponse{
 		OK:      available,
 		Message: "",
+		StartDate: sd,
+		EndDate: ed,
+		RoomID: strconv.Itoa(roomID),
 	}
 
 	out, err := json.MarshalIndent(resp, "", "     ")
