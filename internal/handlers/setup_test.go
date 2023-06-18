205df0592e525e4cb5d079cca5074886e1075368
selim <salimmia3745@gmail.com>
2023-06-18 17:32:20 +0600

    Updating our tests (133)

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -45,6 +45,12 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan =  mailChan
+	defer close(mailChan)
+
+	listenForMail()
+
 	tc, err := CreateTestTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
@@ -58,6 +64,14 @@ func TestMain(m *testing.M) {
 	render.NewRenderer(&app)
 
 	os.Exit(m.Run())
+}
+
+func listenForMail(){
+	go func(){
+		for{
+			_ = <- app.MailChan
+		}
+	}()
 }
 
 func getRoutes() http.Handler {
