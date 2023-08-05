041f02fb06c97bbc1dea06274edeae3d6d0d752b
selim <salimmia3745@gmail.com>
2023-08-05 17:17:25 +0600

    Single Email

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -217,19 +217,19 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	m.App.MailChan <- msg
 
 	// send notification to property owner
-	htmlMessage = fmt.Sprintf(`
-		<strong>Reservation Notification</strong><br>
-		A reservation has been made for %s from %s to %s.
-		`, reservation.Room.RoomName, reservation.StartDate.Format("2006-01-02"), reservation.EndDate.Format("2006-01-02"))
+	// htmlMessage = fmt.Sprintf(`
+	// 	<strong>Reservation Notification</strong><br>
+	// 	A reservation has been made for %s from %s to %s.
+	// 	`, reservation.Room.RoomName, reservation.StartDate.Format("2006-01-02"), reservation.EndDate.Format("2006-01-02"))
 
-	msg = models.MailData{
-		To:      "me@here.com",
-		From:    "me@here.com",
-		Subject: "Reservation Notification",
-		Content: htmlMessage,
-	}
+	// msg = models.MailData{
+	// 	To:      "me@here.com",
+	// 	From:    "me@here.com",
+	// 	Subject: "Reservation Notification",
+	// 	Content: htmlMessage,
+	// }
 
-	m.App.MailChan <- msg
+	// m.App.MailChan <- msg
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
 
