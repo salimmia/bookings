9a9a810460db8f6b730f218a4d2088b77fa28c45
Muhammad Selim <46414940+salimmia@users.noreply.github.com>
2023-08-29 20:04:45 +0600

    Update handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1054,7 +1054,7 @@ func (m *Repository) AdminPostReservationsCalendar(w http.ResponseWriter, r *htt
 	year, _ := strconv.Atoi(r.Form.Get("y"))
 	month, _ := strconv.Atoi(r.Form.Get("m"))
 
-	// process blocks
+	// process block
 	rooms, err := m.DB.AllRooms()
 	if err != nil {
 		helpers.ServerError(w, err)
