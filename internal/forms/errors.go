9228ddc067c6b41ad0306da724bddb716b8325f6
Muhammad Selim <46414940+salimmia@users.noreply.github.com>
2023-09-12 23:12:07 +0600

    Update errors.go

--- internal/forms/errors.go
+++ internal/forms/errors.go
@@ -3,6 +3,7 @@ package forms
 type errors map[string][]string
 
 // Add adds an error message for a given form field
+
 func (e errors) Add(field, message string) {
 	e[field] = append(e[field], message)
 }
